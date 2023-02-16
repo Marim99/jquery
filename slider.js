88a601d825f913c2a5b092cdc9a2ec41948daaf1
marim99 <marim.khaled99@gmail.com>
2023-02-16 15:15:31 +0200

    first commit

---
+++ slider.js
@@ -0,0 +1,22 @@
+$(function () {
+  console.log("hi");
+  let slider = setInterval(swapImages, 1000);
+  function swapImages() {
+    var active = $(".active");
+    var next =
+      $(".active").next().length > 0
+        ? $(".active").next()
+        : $(".slider-container div:first");
+    active.removeClass("active");
+    next.addClass("active");
+  }
+  $(".stop-btn").on("click", () => {
+    clearInterval(slider);
+  });
+
+  $(".task2").on("click", (e) => {
+    if (e.target.classList.contains("img")) {
+      $(e.target).next(".content").slideDown("slow");
+    }
+  });
+});
